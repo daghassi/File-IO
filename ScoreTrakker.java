00b3e43af4ed10ee6c966de16f2791051ff7b454
daghassi <79544966+daghassi@users.noreply.github.com>
2024-09-27 16:57:37 -0600

    created outline

--- ScoreTrakker.java
+++ ScoreTrakker.java
@@ -1,2 +1,35 @@
+import java.io.FileReader;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class ScoreTrakker {
+    private ArrayList<Student> students;
+
+    public void loadDataFile(String filename){
+        FileReader reader = new FileReader(filename);
+        Scanner in = new Scanner(reader);
+        PrintWriter out = new PrintWriter(filename);
+        int lineNumber = 1;
+        while (in.hasNextLine()) {
+            String line = in.nextLine();
+            out.println("/* " + lineNumber + " */ " + line);
+            lineNumber++;
+        }
+        out.close();
+        return;
+    }
+    public void printInOrder(){
+        return;
+    }
+    public void processFiles(){
+        loadDataFile("scores.txt");
+        printInOrder();
+    }
+
+    public static void main(String[] args) {
+        ScoreTrakker scoreTrakker = new ScoreTrakker();
+        scoreTrakker.processFiles();
+    }
+
 }
--- Student.java
+++ Student.java
@@ -1,3 +1,24 @@
-public class Student {
-    //test
+import java.util.ArrayList;
+
+public class Student implements Comparable<Student>{
+    String name;
+    int score;
+
+    public class(String name, int score){
+        this.name = name;
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+    @Override
+    public int compareTo(Student otherStudent){
+        return Integer.compare(this.score, otherStudent.score);
+    }
+
+    @Override
+    public String toString() {
+        return name + " " + score + "\n";
+    }
 }
